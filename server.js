068f4bb274391537aad31520e5804abd0bfb1485
Dave Regg <dave.r.regg@gmail.com>
2020-06-30 11:51:30 -0400

    Removed default route

--- server.js
+++ server.js
@@ -11,8 +11,4 @@ connectDb();
 // Routes
 app.use(`/api/users`, require(`./routes/api/users`));
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
-
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
